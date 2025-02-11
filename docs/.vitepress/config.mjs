b6b58c6795e6e57b796360b8dfcb3d511b10847c
Judu <break_limit@sina.cn>
2025-02-11 13:52:41 +0800

    test

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,100 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    title: "Judu's Wiki",
+    description: "学习、记录",
+    lang: 'zh-CN',
+    head: [['link', { rel: 'icon', href: '/icon.ico' }]],
+    srcDir: './src',
+    locales: {
+
+        root: {
+            label: '简体中文',
+            lang: 'zh'
+        },
+        // en: {
+        //     label: "English",
+        //     lang: 'en'
+        // }
+    },
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+
+        search: {
+            provider: 'local',
+            options: {
+                locales: {
+                    root: {
+                        translations: {
+                            button: {
+                                buttonText: '搜索文档',
+                                buttonAriaLabel: '搜索文档'
+                            },
+                            modal: {
+                                noResultsText: '无法找到相关结果',
+                                resetButtonTitle: '清除查询条件',
+                                footer: {
+                                    selectText: '选择',
+                                    navigateText: '切换',
+                                    closeText: '关闭'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '嵌入式 & 软件开发', link: '/Linux/' }
+        ],
+
+        sidebar: [
+            {
+                text: '嵌入式 & 软件开发',
+                items: [
+                    {
+                        text: 'Linux',
+                        items: [
+                            // {
+                            //     text: 'Linux 驱动开发基础',
+                            //     link: '/Linux/LinuxDriver/',
+                            //     collapsed: true,
+                            //     items: [
+                            //         {text:'a',link:'a.md'},
+                            //         {text:'b',link:'b.md'},
+                            //     ]
+                            // },
+                            // {
+                            //     text: 'Linux 应用开发基础',
+                            //     link: '/Linux/LinuxApp/',
+                            //     collapsed: true,
+                            //     items: [
+                            //         {text:'a',link:''},
+                            //         {text:'b',link:''},
+                            //     ]
+                            // },
+                            {
+                                text: 'Linux 基础知识',
+                                // link: '/Linux/LinuxNotes/',
+                                collapsed: true,
+                                items: [
+                                    { text: 'tar命令', link: '/Linux/LinuxNotes/tar' },
+                                    { text: 'tmux', link: '/Linux/LinuxNotes/tmux' },
+                                ]
+                            }
+                        ],
+                        link: '/Linux/',
+                        collapsed: true
+                    }
+                ]
+            }
+        ],
+
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/juduAB' }
+        ],
+    }
+})
---
+++ docs/src/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Judu's Wiki"
+#   text: "学习、记录"
+  tagline: Hi there~ 欢迎来到我的知识库。
+  actions:
+    - theme: brand
+      text: 嵌入式 & 软件开发
+      link: /Linux
+#     - theme: alt
+#       text: API Examples
+#       link: /api-examples
+
+# features:
+#   - title: 嵌入式 & 软件开发
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
