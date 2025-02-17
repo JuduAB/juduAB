12aee10da2a35e8d854c20502f0c7397bc5880c2
Judu <break_limit@sina.cn>
2025-02-17 17:34:33 +0800

    多核处理器AMP/SMP/BMP介绍

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -69,7 +69,8 @@ export default defineConfig({
                                 { text: 'tmux', link: '/ESWDev/LinuxNotes/tmux' },
                                 { text: '257&DEP', link: '/ESWDev/LinuxNotes/257&DEP' },
                                 { text: '多线程编程', link: '/ESWDev/LinuxNotes/多线程编程' },
-                                { text: '虚拟机磁盘扩展', link: '/ESWDev/LinuxNotes/虚拟机磁盘扩展' }
+                                { text: '虚拟机磁盘扩展', link: '/ESWDev/LinuxNotes/虚拟机磁盘扩展' },
+                                { text: '多核处理器基础', link: '/ESWDev/LinuxNotes/多核处理器基础' },
                             ],
                         },
                         {
